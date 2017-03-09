d65452f49f02d871736d358e618cf62f6d1c0e8a
Gabriel Scherer <gabriel.scherer@gmail.com>
2017-03-08 23:59:40 -0500

    a small script to markdownify Changes fragments

---
+++ markdownify.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat \
+| sed "s,GPR#\\([0-9]*\\),[GPR~#~\\1](https://github.com/ocaml/opam-repository/pull/\\1),g" \
+| sed "s,MPR#\\([0-9]*\\),[PR~#~\\1](https://caml.inria.fr/mantis/view.php?id=\\1),g" \
+| sed "s,PR#\\([0-9]*\\),[PR~#~\\1](https://caml.inria.fr/mantis/view.php?id=\\1),g" \
+| sed "s,PR~#~,PR#,g" \
+
